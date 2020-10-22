618126fd309a0849d7466a6c060b09829929e99c
Leslie-Raven <Leslie-Raven@DESKTOP-7KV2OH2>
2020-10-23 06:03:56 +0800

    8.6 Using @Qualifier with Constructors

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -7,13 +7,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
-	@Autowired
-	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
 	//define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	@Autowired
+	public TennisCoach(@Qualifier("randomFortuneService") FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside constructor using @autowired and @qualifier");
+		
+		fortuneService = theFortuneService;
 	}
 	
 	/*
