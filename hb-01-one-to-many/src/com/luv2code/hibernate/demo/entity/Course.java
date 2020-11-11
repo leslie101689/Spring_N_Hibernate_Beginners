bc00924ab16634fb5dd654a35576b20e5247bdae
leslie101689 <leslie101689@hotmail.com>
2020-11-11 18:13:31 +0800

    5. Define Course Relationship

--- hb-01-one-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
+++ hb-01-one-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,6 +1,13 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -8,15 +15,22 @@ import javax.persistence.Table;
 public class Course {
 
 	// define our fields
-	// define contructors
+	// define constructors
 	// define getter setters
 	// define toString()
 	// annotate fields
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	
+	@Column(name="title")
 	private String title;
 	
+	@ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE,
+						  CascadeType.DETACH,CascadeType.REFRESH})
+	@JoinColumn(name="insturctor_id")
 	private Instructor instructor;
 	
 	public Course() {
