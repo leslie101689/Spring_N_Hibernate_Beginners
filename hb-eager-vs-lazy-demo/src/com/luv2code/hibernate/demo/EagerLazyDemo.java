1653e20976cb1050a95f4db5f7613413a76f1606
leslie101689 <leslie101689@hotmail.com>
2020-11-12 06:41:39 +0800

    5.Coding - Closing the session

--- hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/EagerLazyDemo.java
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/EagerLazyDemo.java
@@ -34,12 +34,17 @@ public class EagerLazyDemo {
 			Instructor tempInstructor = session.get(Instructor.class, theId);
 			
 			System.out.println("luv2code: Instructor: " + tempInstructor);
-			
-			// get course for the instructor
-			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
-			
+				
 			// commit transaction
 			session.getTransaction().commit();
+			
+			// close the session
+			session.close();
+			
+			// since courses are lazy loaded... this should fail
+			
+			// get course for the instructor
+						System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
 			
 			System.out.println("luv2code: Done!");
 		}
