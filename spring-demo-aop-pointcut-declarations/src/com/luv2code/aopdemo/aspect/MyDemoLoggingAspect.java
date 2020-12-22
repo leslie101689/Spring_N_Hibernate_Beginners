a0a4202e9ccf57a209a4a86b773e37d907ff65ca
leslie101689 <leslie101689@hotmail.com>
2020-12-22 15:30:10 +0800

    5. AOP Combining Pointcuts - Write Some Code - Part 2

--- spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,21 +8,29 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-
-	// this is where we add all of our related advices for logging
-	
-	// let's start with an @Before advice
 	
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	private void forDAOPackage() {}
 	
-	@Before("forDAOPackage()")
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// create point: include package ... exclude getter/setter
+	@Pointcut("forDAOPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n====>>> Execution @Before advice on method");
 	}
 	
-	@Before("forDAOPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 		System.out.println("\n====>>> Performing API analytics");
 	}
